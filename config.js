9ec8e21e6ecd97d2cb3032bc91eb50177fcf7512
hostdeployment-bit <hostdeployment@gmail.com>
2026-03-15 15:10:31 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "POPKID~", 
+    PREFIX: process.env.PREFIX || ".",
+    OWNER_NUMBER: ["254732297194"],
+    OWNER_NAME: "Popkid Kenya",
+    AUTO_READ_STATUS: "true",
+    AUTO_REACT_STATUS: "true",
+    AUTO_BIO: "true"
+}
